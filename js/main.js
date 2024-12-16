7f18ac705006c58a0ffc646c5d6d6ae873e552c0
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2024-12-16 12:00:47 +0000

    feat: add contact info and accordion

--- js/main.js
+++ js/main.js
@@ -9,7 +9,9 @@ import { initCarousel } from './modules/carousel.js';
 import { initCookies } from "./modules/cookies.js";
 import { initSidebar } from './modules/sidebar.js';
 import { initSticky } from './modules/sticky.js';
+import { initAccordion } from './modules/accordion.js';
 
+initAccordion();
 initCarousel();
 initCookies();
 initSidebar();
---
+++ js/modules/accordion.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// js/accordion.js
+// ==========================================================================
+
+const $contactAccordionWrapper = document.querySelector('.contact__accordion-wrapper');
+const $contactAccordionPanel = document.querySelector('.contact__accordion-panel');
+
+export function initAccordion() {
+    $contactAccordionWrapper.addEventListener('click', togglePanel);
+}
+
+// Show or hide panel by setting max height.
+function togglePanel() {
+    if ($contactAccordionPanel.style.maxHeight) {
+        $contactAccordionPanel.style.maxHeight = null;
+      } else {
+        $contactAccordionPanel.style.maxHeight = $contactAccordionPanel.scrollHeight + "px";
+      }
+}
