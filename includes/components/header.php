cd0dcb58bcf68df21524aca481bd5ca1eb3e7284
JasonFurmage <150832588+JasonFurmage@users.noreply.github.com>
2025-01-07 23:57:10 +0000

    feat: set maximum scale to 1 to prevent scaling

--- includes/components/header.php
+++ includes/components/header.php
@@ -8,7 +8,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0">
     <link rel="stylesheet" href="assets/css/main.css">
     <title><?php echo $pageTitle ?? 'Full Service Digital Agency | Cambridgeshire & Norfolk | Netmatters'; ?></title>
     <script src="assets/js/main.js" type="module"></script>
